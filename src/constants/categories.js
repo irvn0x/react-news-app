3377719cf345e6fdc50855b702e4474f1036bfd4
Irvansyah <ilhamirvansyah.dev@gmail.com>
2023-09-13 13:29:05 +0700

    create constant category

---
+++ src/constants/categories.js
@@ -0,0 +1,30 @@
+export const CATEGORIES = [
+  {
+    name: "BBC News",
+    slug: "bbc news",
+  },
+  {
+    name: "Netflix",
+    slug: "netflix",
+  },
+  {
+    name: "Apple",
+    slug: "apple",
+  },
+  {
+    name: "Google",
+    slug: "google",
+  },
+  {
+    name: "Microsoft",
+    slug: "microsoft",
+  },
+  {
+    name: "Bitcoin",
+    slug: "bitcoin",
+  },
+  {
+    name: "Blockchain",
+    slug: "blockchain",
+  },
+];
